1fa7786ec9af0efbf47ec4e38ff827f89ea9a9a9
locohammerhead <locohammerhead@gmail.com>
2018-02-14 14:30:49 -0500

    Update WBXcommander.sh

--- WBXcommander.sh
+++ WBXcommander.sh
@@ -3,8 +3,8 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #                                                     #
 #                 WBX Commander Script                #
-#                by locohammerhead,cyrus19901 				#
-#														and                				#
+#                by locohammerhead,cyrus19901 	      #
+#			   and                	      #
 #           tharude a.k.a The Forging Penguin         #
 #         thanks ViperTKD for the helping hand        #
 #                 19/01/2017 ARK Team                 #
